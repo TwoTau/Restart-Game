46b9fc7a5d9c9ce9b45314a82d133434cd15e710
TwoTau <TwoTau@users.noreply.github.com>
2017-06-07 17:56:04 -0700

    Two player game

--- app/src/main/java/org/spartabots/restart/DrawingView.java
+++ app/src/main/java/org/spartabots/restart/DrawingView.java
@@ -4,161 +4,159 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
+import android.graphics.Point;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import java.util.zip.Inflater;
+import java.util.LinkedList;
+
 
 public class DrawingView extends View {
     private Paint paint;
-    private int[][] board = new int[21][21];
+
+    private int width = 15;
+    private int height = width;
 
     private int cellWidth;
 
-    private int playerX = 10;
-    private int playerY = 20;
+    public int playerNumber = 0;
 
-    private int levelNumber = 0;
-
-    private boolean revealEverything = true;
-
-    private final int EMPTY_CODE = 0;
-    private final int DOOR_CODE = 1;
-    private final int KILLBOX_CODE = 2;
-    private final int TELEPORTER_CODE = 3;
-    private final int BACK_CODE = 4;
-    private final int LOCKDOOR_CODE = 5;
-    private final int KEY_CODE = 6;
-
-    private boolean key = false;
+    public LinkedList<Point> player1 = new LinkedList<Point>();
+    public LinkedList<Point> player2 = new LinkedList<Point>();
 
     private Context c;
     public DrawingView(Context context, int width) {
         super(context);
         c = context;
-        cellWidth = width / 21;
+        cellWidth = width / 15;
 
-        loadLevel();
+        player1.add(new Point(0, 4));
+        player1.add(new Point(0, 3));
+        player1.add(new Point(0, 2));
+        player1.add(new Point(0, 1));
+        player1.add(new Point(0, 0));
+
+        player2.add(new Point(14, 10));
+        player2.add(new Point(14, 11));
+        player2.add(new Point(14, 12));
+        player2.add(new Point(14, 13));
+        player2.add(new Point(14, 14));
 
         paint = new Paint();
         paint.setColor(0xFFDDDDDD);
         paint.setAntiAlias(true);
-
-
     }
     @Override
     public void onDraw(Canvas canvas) {
 
         canvas.drawColor(Color.rgb(8, 8, 8));
 
-        paint.setColor(0xFFFFFFFF);
-        canvas.drawCircle((int) (cellWidth * (playerX + 0.5)), (int) (cellWidth * (playerY + 0.5)), cellWidth / 2, paint);
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[i].length; j++) {
-                if(board[i][j] == DOOR_CODE) { // door
-                    paint.setColor(0xFF43DD53);
-                    canvas.drawCircle((int) (cellWidth * (j+0.5)), (int) (cellWidth * (i+0.5)), cellWidth / 2, paint);
-                }
-                else if(revealEverything) {
-                    if(board[i][j] == EMPTY_CODE) { // empty
-                        paint.setColor(0xFF333333);
-                        canvas.drawCircle((int) (cellWidth * (j+0.5)), (int) (cellWidth * (i+0.5)), cellWidth / 2, paint);
-                    }
-                    else if(board[i][j] == KILLBOX_CODE) {
-                        paint.setColor(0xFFEE1133);
-                        canvas.drawCircle((int) (cellWidth * (j+0.5)), (int) (cellWidth * (i+0.5)), cellWidth / 2, paint);
-                    }
-                    else if(board[i][j] == TELEPORTER_CODE) {
-                        paint.setColor(0xFF4466EE);
-                        canvas.drawCircle((int) (cellWidth * (j+0.5)), (int) (cellWidth * (i+0.5)), cellWidth / 2, paint);
-                    }
-                }
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                paint.setColor(0xFF333333);
+                canvas.drawCircle((int) (cellWidth * (j+0.5)), (int) (cellWidth * (i+0.5)), cellWidth / 2, paint);
             }
         }
 
-        paint.setColor(0xFFEEEEEE);
-        canvas.drawCircle((int) (cellWidth * (playerX+0.5)), (int) (cellWidth * (playerY+0.5)), cellWidth / 2, paint);
-        playerTrack();
+        for(int i = 0; i < player1.size(); i++) {
+            Point p = player1.get(i);
+            if(i == 0) {
+                paint.setColor(0xFFEE0044);
+            } else {
+                paint.setColor(0xFFAA2266);
+            }
+            canvas.drawCircle((int) (cellWidth * (p.x+0.5)), (int) (cellWidth * (p.y+0.5)), cellWidth / 2, paint);
+        }
+        for(int i = 0; i < player2.size(); i++) {
+            Point p = player2.get(i);
+            if(i == 0) {
+                paint.setColor(0xFF4411EE);
+            } else {
+                paint.setColor(0xFF7733AA);
+            }
+            canvas.drawCircle((int) (cellWidth * (p.x+0.5)), (int) (cellWidth * (p.y+0.5)), cellWidth / 2, paint);
+        }
+
         invalidate();
     }
-    public void movePlayerLeft() {
-        if(playerX > 0) {
-            --playerX;
-        }
-    }
-    public void movePlayerRight() {
-        if(playerX < board[0].length - 1) {
-            ++playerX;
-        }
-    }
-    public void movePlayerDown() {
-        if(playerY < board.length - 1) {
-            ++playerY;
-        }
-    }
-    public void movePlayerUp() {
-        if(playerY > 0) {
-            --playerY;
-        }
-    }
-    public void setPlayerPos(int newX, int newY) {
-        playerX = newX;
-        playerY = newY;
-    }
-    private void resetBoard() {
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[i].length; j++) {
-                board[i][j] = EMPTY_CODE;
+
+    private boolean isIntersecting(LinkedList<Point> ll1, LinkedList<Point> ll2) {
+        for(int i = 0; i < ll1.size(); i++) {
+            for(int j = 0; j < ll2.size(); j++) {
+                if(ll1.get(i) == ll2.get(j)) {
+                    return true;
+                }
             }
         }
-
-        setPlayerPos(3, 6);
-
-        board[0][3] = DOOR_CODE; // door
+        return false;
     }
 
-    private void loadLevel() {
-        resetBoard();
-        String levelName = "Level:" + levelNumber;
-        Toast.makeText(c, levelName, Toast.LENGTH_SHORT).show();
-        if(levelNumber == 1) {
-            // do nothing
-        }
-        else if(levelNumber == 2) { // cell in front of player is killer
-            board[0][3] = LOCKDOOR_CODE;
-            int randomX = (int) Math.random()*board[0].length;
-            int randomY = (int) Math.random()*board.length;
-            board[randomX][randomY] = KEY_CODE;
-        }
-        else if(levelNumber == 3) { // cell in front of player is teleporter
-            board[5][3] = BACK_CODE;
-        }
-    }
-    private void playerTrack(){
-        System.out.println("hallo");
-        if(board[playerY][playerX] == KILLBOX_CODE){
-            loadLevel();
-        } else if(board[playerY][playerX] == DOOR_CODE){
-            levelNumber++;
-            loadLevel();
-        } else if(board[playerY][playerX] == BACK_CODE) {
-
-        } else if (board[playerY][playerX] == TELEPORTER_CODE) {
-
-        } else if(board[playerY][playerX] == LOCKDOOR_CODE) {
-            Toast.makeText(c, "Door is locked. Find the key.", Toast.LENGTH_SHORT).show();
-            if(key){
-                key = false;
-                levelNumber++;
-                loadLevel();
+    private boolean isIntersecting(Point head, LinkedList<Point> ll1) {
+        for(int i = 1; i < ll1.size(); i++) {
+            if(head == ll1.get(i)) {
+                return true;
             }
-        } else if(board[playerY][playerX] == KEY_CODE){
-            Toast.makeText(c, "Key Acquired", Toast.LENGTH_SHORT).show();
-            key = true;
-            board[playerY][playerX] = EMPTY_CODE;
         }
+        return false;
+    }
+
+    public LinkedList<Point> getThisPlayer() {
+        return (playerNumber == 1) ? player1 : player2;
+    }
+
+    public boolean movePlayerLeft() {
+        Point head = getThisPlayer().getFirst();
+        if(head.x > 0 && !isIntersecting(new Point(head.x - 1, head.y), getThisPlayer())) {
+            Point last = getThisPlayer().removeLast();
+            last.set(head.x, head.y);
+            getThisPlayer().addFirst(last);
+            --last.x;
+            return true;
+        }
+        return false;
+    }
+    public boolean movePlayerRight() {
+        Point head = getThisPlayer().getFirst();
+        if(head.x < width - 1 && !isIntersecting(new Point(head.x + 1, head.y), getThisPlayer())) {
+            Point last = getThisPlayer().removeLast();
+            last.set(head.x, head.y);
+            getThisPlayer().addFirst(last);
+            ++last.x;
+            return true;
+        }
+        return false;
+    }
+    public boolean movePlayerDown() {
+        Point head = getThisPlayer().getFirst();
+        if(head.y < height - 1 && !isIntersecting(new Point(head.x, head.y + 1), getThisPlayer())) {
+            Point last = getThisPlayer().removeLast();
+            last.set(head.x, head.y);
+            getThisPlayer().addFirst(last);
+            ++last.y;
+            return true;
+        }
+        return false;
+    }
+    public boolean movePlayerUp() {
+        Point head = getThisPlayer().getFirst();
+        if(head.y > 0  && !isIntersecting(new Point(head.x, head.y - 1), getThisPlayer())) {
+            Point last = getThisPlayer().removeLast();
+            last.set(head.x, head.y);
+            getThisPlayer().addFirst(last);
+            --last.y;
+            return true;
+        }
+        return false;
+    }
+
+    public String getJSONPosition() {
+        LinkedList<Point> player =  getThisPlayer();
+        String json = "{\"position\":[";
+        for(int i = 0; i < player.size(); i++) {
+            json += "[" + player.get(i).x + "," + player.get(i).y + "]";
+            if(i < player.size() - 1) {
+                json += ",";
+            }
+        }
+        return json + "]}";
     }
 }
--- app/src/main/java/org/spartabots/restart/MainActivity.java
+++ app/src/main/java/org/spartabots/restart/MainActivity.java
@@ -1,22 +1,43 @@
 package org.spartabots.restart;
 
+import android.graphics.Point;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawingView drawingView;
 
+    public boolean myTurn = false;
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://192.168.129.8:2976");
+        } catch (URISyntaxException e) {}
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        DisplayMetrics metrics = getApplicationContext().getResources().getDisplayMetrics();
-        int width = metrics.widthPixels;
-        int height = metrics.heightPixels;
+        int width = getApplicationContext().getResources().getDisplayMetrics().widthPixels;
 
         drawingView = new DrawingView(this, width);
 
@@ -24,20 +45,122 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(drawingView);
 
+        connectWebSocket();
+
         drawingView.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
             public void onSwipeTop() {
-                drawingView.movePlayerUp();
+                if(myTurn) {
+                    drawingView.movePlayerUp();
+                    endTurn();
+                }
             }
             public void onSwipeRight() {
-                drawingView.movePlayerRight();
+                if(myTurn) {
+                    drawingView.movePlayerRight();
+                    endTurn();
+                }
             }
             public void onSwipeLeft() {
-                drawingView.movePlayerLeft();
+                if(myTurn) {
+                    drawingView.movePlayerLeft();
+                    endTurn();
+                }
             }
             public void onSwipeBottom() {
-                drawingView.movePlayerDown();
+                if(myTurn) {
+                    drawingView.movePlayerDown();
+                    endTurn();
+                }
             }
 
         });
     }
+
+    private void connectWebSocket() {
+        mSocket.on("setup", onSetup);
+        mSocket.on("gamestart", onGamestart);
+        mSocket.on("updatepos", onUpdate);
+        mSocket.connect();
+    }
+
+    private void endTurn() {
+        mSocket.emit("position", drawingView.getJSONPosition());
+        myTurn = false;
+    }
+
+    private Emitter.Listener onSetup = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            int data = 0;
+            try {
+                JSONObject data2 = new JSONObject(args[0].toString());
+                data = data2.getInt("playernumber");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            drawingView.playerNumber = data;
+            System.out.println("Socket: SETUP COMPLETE. I am #" + data);
+        }
+    };
+
+    private Emitter.Listener onGamestart = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            JSONArray data = new JSONArray();
+            try {
+                JSONObject data2 = new JSONObject(args[0].toString());
+                data = data2.getJSONArray("player" + ((drawingView.playerNumber == 1) ? "2": "1"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            if(drawingView.playerNumber == 1) {
+                myTurn = true;
+            }
+
+            System.out.println("Socket: GAME STARTED: " + data);
+        }
+    };
+
+    private Emitter.Listener onUpdate = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            JSONArray data = new JSONArray();
+            int turnNumber = 0;
+            try {
+                JSONObject data2 = new JSONObject(args[0].toString());
+                data = data2.getJSONArray("player" + ((drawingView.playerNumber == 1) ? "2": "1"));
+                turnNumber = data2.getInt("turn");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            if((turnNumber % 2 == 0 && drawingView.playerNumber == 2) || (turnNumber % 2 == 1 && drawingView.playerNumber == 1)) {
+                myTurn = true;
+            }
+
+            LinkedList<Point> updatedPlayer = new LinkedList<Point>();
+
+            for(int i = 0; i < data.length(); i++) {
+                try {
+                    String[] arrayThing = data.get(i).toString().split(",");
+                    int x = Integer.parseInt(arrayThing[0].substring(1, arrayThing[0].length()));
+                    int y = Integer.parseInt(arrayThing[1].substring(0, arrayThing[1].length() - 1));
+                    updatedPlayer.add(new Point(x, y));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(drawingView.playerNumber == 1) {
+                drawingView.player2 = updatedPlayer;
+            } else {
+                drawingView.player1 = updatedPlayer;
+            }
+
+            System.out.println("Socket: UPDATED: " + data);
+        }
+    };
+
+
 }
